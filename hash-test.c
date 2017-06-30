4f9080c240d19115246923afd4072191f2406265
Alexei A. Smekalkine <ikle@ikle.ru>
2017-07-01 01:24:39 +0300

    hash-test: add speed test procedure

--- hash-test.c
+++ hash-test.c
@@ -21,7 +21,7 @@ static int error (const char *message, int system)
 
 static int usage (void)
 {
-	fprintf (stderr, "usage:\n\thash-test <algorithm> [data]\n");
+	fprintf (stderr, "usage:\n\thash-test <algorithm> [[-s] data]\n");
 
 	return 1;
 }
@@ -48,17 +48,55 @@ static const struct hash_core *find_core (const char *name)
 	return NULL;
 }
 
+#include <sys/times.h>
+#include <unistd.h>
+
+#define COUNT	(1000000)
+
+static double ticks_to_secs (double ticks)
+{
+	int count;
+
+	if ((count = sysconf (_SC_CLK_TCK)) == -1)
+		count = 100;
+
+	return ticks / count;
+}
+
+static void test_speed (struct hash *h, const char *string, void *out)
+{
+	const size_t len = strlen (string);
+	struct tms t0, t1;
+	int i;
+	double duration;
+
+	times (&t0);
+
+	for (i = 0; i < COUNT; ++i) {
+		hash_update (h, string, len);
+		hash_final (h, out);
+	}
+
+	times (&t1);
+	duration = ticks_to_secs ((double) t1.tms_utime - t0.tms_utime);
+
+	printf ("hash/s = %f\n", COUNT / duration);
+}
+
+#define get_arg()	(argc < 2 ? NULL : (--argc, ++argv, argv[0]))
+
 int main (int argc, char *argv[])
 {
+	const char *arg;
 	const struct hash_core *core;
 	struct hash *h;
 	char buf[BUFSIZ];
 	size_t len;
 
-	if (argc < 2 || argc > 3)
+	if ((arg = get_arg ()) == NULL)
 		return usage ();
 
-	if ((core = find_core (argv[1])) == NULL)
+	if ((core = find_core (arg)) == NULL)
 		return error ("unknown algorithm", 0);
 
 	if (sizeof (buf) < core->hash_size)
@@ -67,13 +105,22 @@ int main (int argc, char *argv[])
 	if ((h = hash_alloc (core)) == NULL)
 		return error ("cannot initialize algorithm", 1);
 
-	if (argc == 3)
-		hash_update (h, argv[2], strlen (argv[2]));
-	else
-		while ((len = fread (buf, 1, sizeof (buf), stdin)) > 0)
-			hash_update (h, buf, len);
+	if ((arg = get_arg ()) != NULL && strcmp (arg, "-s") == 0) {
+		if ((arg = get_arg ()) == NULL)
+			return usage ();
 
-	hash_final (h, buf);
+		test_speed (h, arg, buf);
+	}
+	else {
+		if (arg != NULL)
+			hash_update (h, arg, strlen (arg));
+		else
+			while ((len = fread (buf, 1, sizeof (buf), stdin)) > 0)
+				hash_update (h, buf, len);
+
+		hash_final (h, buf);
+	}
+
 	hash_free (h);
 
 	show (buf, core->hash_size);
