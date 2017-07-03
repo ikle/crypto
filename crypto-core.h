ec50ce1685043625d25447a2e0b31c77458ea923
Alexei A. Smekalkine <ikle@ikle.ru>
2017-07-04 01:52:46 +0300

    crypto-core: add basic interface for block ciphers

--- crypto-core.h
+++ crypto-core.h
@@ -25,6 +25,10 @@ struct crypto_core {
 	int (*get) (const void *state, int type, ...);
 	int (*set) (void *state, int type, ...);
 
+	/* encrypt/decrypt one block of data */
+	void (*encrypt) (void *state, const void *in, void *out);
+	void (*decrypt) (void *state, const void *in, void *out);
+
 	void (*transform) (void *state, const void *block);
 	void (*final) (void *state, const void *in, size_t len, void *out);
 };
