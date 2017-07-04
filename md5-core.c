9abe2b8651eaa64bb505231115442f9378fbdc8a
Alexei A. Smekalkine <ikle@ikle.ru>
2017-07-04 05:44:22 +0300

    md5: unify header comment style

--- md5-core.c
+++ md5-core.c
@@ -1,10 +1,9 @@
 /*
- * RFC 1321: The MD5 Message-Digest Algorithm
+ * The MD5 Message-Digest Algorithm
  *
- * MD5 Core
+ * Copyright (c) 2017 Alexei A. Smekalkine <ikle@ikle.ru>
  *
- * Copyright (c) 2017 Alexei A. Smekalkine
- *
+ * Standards: RFC 1321
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
