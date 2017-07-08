20556792ff54aad0176267a2d98a2f4cd0bf92b3
Alexei A. Smekalkine <ikle@ikle.ru>
2017-07-08 11:58:13 +0300

    cfb: add cipher feedback core

---
+++ include/mop/cfb.h
@@ -0,0 +1,17 @@
+/*
+ * CFB: Cipher Feedback
+ *
+ * Copyright (c) 2011-2017 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * Standard: NIST FIPS 81, NIST SP 800-38A, GOST R 34.13-2015
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef CRYPTO_CFB_CORE_H
+#define CRYPTO_CFB_CORE_H  1
+
+#include <crypto-core.h>
+
+extern const struct crypto_core cfb_core;
+
+#endif  /* CRYPTO_CFB_CORE_H */
---
+++ mop/cfb.c
@@ -0,0 +1,48 @@
+/*
+ * CFB: Cipher Feedback
+ *
+ * Copyright (c) 2011-2017 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * Standard: NIST FIPS 81, NIST SP 800-38A, GOST R 34.13-2015
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <string.h>
+#include <mop/cfb.h>
+
+#include "mop.h"
+
+static void cfb_encrypt (void *state, const void *in, void *out)
+{
+	struct state *o = state;
+	const size_t bs = o->algo->get (o->cipher, CRYPTO_BLOCK_SIZE);
+
+	o->algo->encrypt (o->cipher, o->iv, o->iv);
+
+	xor_block (in, o->iv, out, bs);
+
+	memcpy (o->iv, out, bs);
+}
+
+static void cfb_decrypt (void *state, const void *in, void *out)
+{
+	struct state *o = state;
+	const size_t bs = o->algo->get (o->cipher, CRYPTO_BLOCK_SIZE);
+
+	o->algo->encrypt (o->cipher, o->iv, o->iv);
+
+	xor_block (in, o->iv, out, bs);
+
+	memcpy (o->iv, in, bs);
+}
+
+const struct crypto_core cfb_core = {
+	.alloc		= mop_alloc,
+	.free		= mop_free,
+
+	.get		= mop_get,
+	.set		= mop_set,
+
+	.encrypt	= cfb_encrypt,
+	.decrypt	= cfb_decrypt,
+};
