0799550f65b917604f569bcf13090b98c8335974
Alexei A. Smekalkine <ikle@ikle.ru>
2017-07-09 03:34:10 +0300

    mop: report hash size equal to block size, support CMAC et co

--- mop/mop.c
+++ mop/mop.c
@@ -101,6 +101,9 @@ int mop_get (const void *state, int type, ...)
 {
 	const struct state *o = state;
 
+	if (type == CRYPTO_HASH_SIZE)
+		type = CRYPTO_BLOCK_SIZE;
+
 	return o->algo->get (o->cipher, type);
 }
 
