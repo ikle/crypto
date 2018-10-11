56d94717d5f0785ead22bda98529d6e9dd500abf
Alexei A. Smekalkine <ikle@ikle.ru>
2018-10-12 00:34:10 +0300

    hash-test: allow to pass transparent data into show method

--- test/hash-test.c
+++ test/hash-test.c
@@ -31,10 +31,12 @@ static int usage (void)
 	return 1;
 }
 
-static void show (const unsigned char *data, size_t len)
+static void show (const void *data, size_t len)
 {
-	for (; len > 0; ++data, --len)
-		printf ("%02x", *data);
+	const unsigned char *p = data;
+
+	for (; len > 0; ++p, --len)
+		printf ("%02x", *p);
 
 	printf ("\n");
 }
