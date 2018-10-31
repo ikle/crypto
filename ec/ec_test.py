c717f6353f325b28f236b7d7f7c66bcc0594a634
Alexei A. Smekalkine <ikle@ikle.ru>
2018-10-31 16:59:09 +0300

    ec/test: use test group from ecgost module

--- ec/ec_test.py
+++ ec/ec_test.py
@@ -18,22 +18,12 @@ rounds = 0
 test_ecgost = True
 test_swj    = True
 
-p  = 0x8000000000000000000000000000000000000000000000000000000000000431 
-a  = 0x7
-b  = 0x5FBFF498AA938CE739B8E022FBAFEF40563F6E6A3472FC2A514C0CE9DAE23B7E 
-
-m  = 0x8000000000000000000000000000000150FE8A1892976154C59CFC193ACCF5B3
-q  = m
-
-Px = 0x2
-Py = 0x8E2A8A0E65147D4BD6316030E16D19C85C97F0A9CA267122B96ABBCEA7E8FC8
+o = ecgost.group ('ecgost-test-a')
 
 d  = 0x7A929ADE789BB9BE10ED359DD39A72C11B60961F49397EEE1D19CE9891EC3B28
 
 e  = 0x2DFBC1B372D89A1188C09C52E0EEC61FCE52032AB1022E8E67ECE6672B043EE5
 k  = 0x77105C9B20BCD3122823C8CF6FCC7B956DE33814E95B7FE64FED924594DCEAB3
-
-curve = ec.Curve (a, b, p)
 
 def test (name, d, P):
 	start = time.clock ()
@@ -46,16 +36,16 @@ def test (name, d, P):
 	print ('{}: {:.2f} ms'.format (name, delta))
 
 for i in range (rounds):
-	P = ec.Point (curve, Px, Py)
+	P = ec.Point (o.curve, o.x, o.y)
 	test ('swa    ', d, P)
 
-	P = ec.SecurePoint (curve, Px, Py)
+	P = ec.SecurePoint (o.curve, o.x, o.y)
 	test ('swa-sec', d, P)
 
-	P = ec_swj.Point (curve, Px, Py)
+	P = ec_swj.Point (o.curve, o.x, o.y)
 	test ('swj    ', d, P)
 
-	P = ec_swj.SecurePoint (curve, Px, Py)
+	P = ec_swj.SecurePoint (o.curve, o.x, o.y)
 	test ('swj-sec', d, P)
 
 	print ()
@@ -66,12 +56,11 @@ if test_ecgost:
 	ecgost.test ()
 
 if test_swj:
-	P = ec_swj.SecurePoint (curve, Px, Py)
+	P = ec_swj.SecurePoint (o.curve, o.x, o.y)
 	Q = d * P
 	C = k * P
 
 	print ('P =', P)
-	print ('qP =', q * P)
+	print ('qP =', o.q * P)
 	print ('Q = dP =', Q)
 	print ('C = kP =', C)
-
